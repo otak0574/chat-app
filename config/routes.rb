94b682b8afd515b0fe769818d08939850454dcf2
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-02-18 13:53:21 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
  #ホームをmessagesにするルーティング
- root to: "messages#index"
+ root to: "rooms#index"
  #ユーザー編集画面が表示されるルーティング
  resources :users, only: [:edit, :update]
  #ルームを新規作成するルーティング
