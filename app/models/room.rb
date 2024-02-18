6e263bfba7aafa99a74d08d8a59fd5716e771eda
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-02-18 17:07:06 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,10 @@
 class Room < ApplicationRecord
   #ルームとユーザーの関係
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   #ユーザーとルームの関係
   has_many :users, through: :room_users #throughオプション
   #ルームとメッセージの関係
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   #バリデーションの記述
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
  #ユーザー編集画面が表示されるルーティング
  resources :users, only: [:edit, :update]
  #ルームを新規作成するルーティング
- resources :rooms, only: [:new, :create] do
+ resources :rooms, only: [:new, :create, :destroy] do
   #メッセージのルーティング
   resources :messages, only: [:index, :create]
  end
