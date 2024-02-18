7de3022177da774dc32e3d321bd9308c4cec5485
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-02-18 13:46:20 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
   #複数のルームは複数のユーザーに紐づけられる
   has_many :room_users
   has_many :users, through: :room_users #throughオプション
+
+  #バリデーションの記述
+  validates :name, presence: true
 end
