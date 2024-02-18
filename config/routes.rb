e4504bee8af5c5e690344cb16e2427045130cf51
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-02-18 11:56:16 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
  root to: "messages#index"
  #ユーザー編集画面が表示されるルーティング
  resources :users, only: [:edit, :update]
+ #ルームを新規作成するルーティング
+ resources :rooms, only: [:new, :create,]
 end
